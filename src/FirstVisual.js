d9e036037e98ff15ec18970cf6d43ed25ab54a1b
arvind pandit <arvindputtu@gmail.com>
2023-05-03 08:31:32 +0000

    Update 4 files

--- src/FirstVisual.js
+++ src/FirstVisual.js
@@ -10,22 +10,222 @@ const dataSource = {
     yaxisname: "Sum of Sales(in Million Pound)",
     showvalues: "1",
     numberprefix: "£",
-    theme: "fusion"
+    theme: "fusion",
   },
   data: [
     {
       label: "Argentina",
-      value: "854851"
+      value: null,
     },
     {
       label: "Australia",
-      value: "611130"
+      value: 854851,
     },
     {
       label: "Austria",
-      value: "1072679"
-    }
-  ]
+      value: 611130,
+    },
+    {
+      label: "Belgium",
+      value: 1072679,
+    },
+    {
+      label: "Bermuda",
+      value: 99667,
+    },
+    {
+      label: "Brazil",
+      value: 1517841,
+    },
+    {
+      label: "Canada",
+      value: 999279,
+    },
+    {
+      label: "Cayman Islands",
+      value: 147565,
+    },
+    {
+      label: "China",
+      value: 23413203,
+    },
+    //  {
+    //    "label": "Colombia",
+    //    "value": 38921
+    //  },
+    //  {
+    //    "label": "Czech Republic",
+    //    "value": 33460
+    //  },
+    //  {
+    //    "label": "Denmark",
+    //    "value": 863142
+    //  },
+    //  {
+    //    "label": "Finland",
+    //    "value": 1149026
+    //  },
+    //  {
+    //    "label": "France",
+    //    "value": 11726498
+    //  },
+    //  {
+    //    "label": "Germany",
+    //    "value": 18413130
+    //  },
+    //  {
+    //    "label": "Greece",
+    //    "value": 17878
+    //  },
+    //  {
+    //    "label": "Hong Kong",
+    //    "value": 270611
+    //  },
+    //  {
+    //    "label": "Hungary",
+    //    "value": 14561
+    //  },
+    //  {
+    //    "label": "Iceland",
+    //    "value": 9904
+    //  },
+    //  {
+    //    "label": "India",
+    //    "value": 2339311
+    //  },
+    //  {
+    //    "label": "Iraq",
+    //    "value": 5888
+    //  },
+    //  {
+    //    "label": "Ireland",
+    //    "value": 1592270
+    //  },
+    //  {
+    //    "label": "Israel",
+    //    "value": 436401
+    //  },
+    //  {
+    //    "label": "Italy",
+    //    "value": 3727633
+    //  },
+    //  {
+    //    "label": "Japan",
+    //    "value": 31601001
+    //  },
+    //  {
+    //    "label": "Liechtenstein",
+    //    "value": 42036
+    //  },
+    //  {
+    //    "label": "Luxembourg",
+    //    "value": 816736
+    //  },
+    //  {
+    //    "label": "Malaysia",
+    //    "value": 297552
+    //  },
+    //  {
+    //    "label": "Malta",
+    //    "value": 5183
+    //  },
+    //  {
+    //    "label": "Mexico",
+    //    "value": 123692
+    //  },
+    //  {
+    //    "label": "Netherlands",
+    //    "value": 4278455
+    //  },
+    //  {
+    //    "label": "New Zealand",
+    //    "value": 125569
+    //  },
+    //  {
+    //    "label": "Norway",
+    //    "value": 1224368
+    //  },
+    //  {
+    //    "label": "Poland",
+    //    "value": 2201
+    //  },
+    //  {
+    //    "label": "Portugal",
+    //    "value": 193891
+    //  },
+    //  {
+    //    "label": "Russia",
+    //    "value": 1331979
+    //  },
+    //  {
+    //    "label": "Saudi Arabia",
+    //    "value": 1177240
+    //  },
+    //  {
+    //    "label": "Singapore",
+    //    "value": 146377
+    //  },
+    //  {
+    //    "label": "Slovenia",
+    //    "value": 14833
+    //  },
+    //  {
+    //    "label": "South Africa",
+    //    "value": 143317
+    //  },
+    //  {
+    //    "label": "South Korea",
+    //    "value": 9108190
+    //  },
+    //  {
+    //    "label": "Spain",
+    //    "value": 2708606
+    //  },
+    //  {
+    //    "label": "Sweden",
+    //    "value": 2116491
+    //  },
+    //  {
+    //    "label": "Switzerland",
+    //    "value": 3966512
+    //  },
+    //  {
+    //    "label": "Taiwan",
+    //    "value": 5125375
+    //  },
+    //  {
+    //    "label": "Thailand",
+    //    "value": 8535
+    //  },
+    //  {
+    //    "label": "Turkey",
+    //    "value": 429871
+    //  },
+    //  {
+    //    "label": "UK",
+    //    "value": 13637030
+    //  },
+    //  {
+    //    "label": "Ukraine",
+    //    "value": 2461
+    //  },
+    //  {
+    //    "label": "United Arab Emirates",
+    //    "value": 59761
+    //  },
+    //  {
+    //    "label": "USA",
+    //    "value": 46973283
+    //  },
+    //  {
+    //    "label": "Venezuela",
+    //    "value": 504837
+    //  },
+    //  {
+    //    "label": "",
+    //    "value": null
+    //  }
+  ],
 };
 
 const FirstVisual = () => {
--- src/FourthVisual.js
+++ src/FourthVisual.js
@@ -8,87 +8,210 @@ charts(FusionCharts);
 const dataSource = {
   chart: {
     caption: "Yearly Sales, Capex, Profit, Market Cap",
-    subcaption: "2010-2013",
+    subcaption: "2010-2020",
     xaxisname: "Years",
     yaxisname: "Total sum of Sales, Capex, Profit, Market Cap in million pound",
     formatnumberscale: "1",
     plottooltext:
       "<b>$dataValue</b> was projected on <b>$seriesName</b> in $label",
-    theme: "candy"
+    theme: "candy",
   },
   categories: [
     {
       category: [
         {
-          label: "2010"
+          label: "2010",
         },
         {
-          label: "2011"
+          label: "2011",
         },
         {
-          label: "2012"
-        }
-      ]
-    }
+          label: "2012",
+        },
+        {
+          label: "2013",
+        },
+        {
+          label: "2014",
+        },
+        {
+          label: "2015",
+        },
+        {
+          label: "2016",
+        },
+        {
+          label: "2017",
+        },
+        {
+          label: "2018",
+        },
+        {
+          label: "2019",
+        },
+        {
+          label: "2020",
+        },
+      ],
+    },
   ],
   dataset: [
     {
       seriesname: "Sales",
       data: [
         {
-          value: "13894163"
+          value: 13894163,
         },
         {
-          value: "15710758"
+          value: 15710758,
         },
         {
-          value: "16854413"
-        }
-      ]
+          value: 16854413,
+        },
+        {
+          value: 16723019,
+        },
+        {
+          value: 17972595,
+        },
+        {
+          value: 17686790,
+        },
+        {
+          value: 17910320,
+        },
+        {
+          value: 18448033,
+        },
+        {
+          value: 20351620,
+        },
+        {
+          value: 21018395,
+        },
+        {
+          value: 18952091,
+        },
+      ],
     },
     {
       seriesname: "Capex",
       data: [
         {
-          value: "764435.7498"
+          value: 764435.7498,
         },
         {
-          value: "892720.585"
+          value: 892720.585,
         },
         {
-          value: "1109092.789"
-        }
-      ]
+          value: 1109092.789,
+        },
+        {
+          value: 1087703.811,
+        },
+        {
+          value: 1131195.571,
+        },
+        {
+          value: 1127502.322,
+        },
+        {
+          value: 1167878.202,
+        },
+        {
+          value: 1151303.479,
+        },
+        {
+          value: 1317342.829,
+        },
+        {
+          value: 1404746.798,
+        },
+        {
+          value: 1293241.058,
+        },
+      ],
     },
     {
       seriesname: "Profits",
       data: [
         {
-          value: "-6576872.37"
+          value: -6576872.37,
         },
         {
-          value: "1702982.03"
+          value: 1702982.03,
         },
         {
-          value: "1549539.65"
-        }
-      ]
+          value: 1549539.65,
+        },
+        {
+          value: 1555429.22,
+        },
+        {
+          value: 1735387.21,
+        },
+        {
+          value: 1518669.57,
+        },
+        {
+          value: 1671173.01,
+        },
+        {
+          value: 1909324.19,
+        },
+        {
+          value: 2275661.44,
+        },
+        {
+          value: 2077564.89,
+        },
+        {
+          value: 1553985.01,
+        },
+      ],
     },
     {
       seriesname: "Market Cap",
       data: [
         {
-          value: "14001791.78"
+          value: 14001791.78,
         },
         {
-          value: "0"
+          value: 0,
         },
         {
-          value: "0"
-        }
-      ]
-    }
-  ]
+          value: 0,
+        },
+        {
+          value: 0,
+        },
+        {
+          value: 0,
+        },
+        {
+          value: 0,
+        },
+        {
+          value: 23081916.37,
+        },
+        {
+          value: 24538463.37,
+        },
+        {
+          value: 27163043.76,
+        },
+        {
+          value: 27269822.28,
+        },
+        {
+          value: 33855136.24,
+        },
+        {
+          value: null,
+        },
+      ],
+    },
+  ],
 };
 
 const FourthVisual = () => {
